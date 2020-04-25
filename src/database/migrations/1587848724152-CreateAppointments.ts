aa867bfd4822d1a954feb448bd85d2e2c88766e3
Shilton <shilton.sistemas@gmail.com>
2020-04-25 18:23:35 -0300

    Creating appointment table/migration

---
+++ src/database/migrations/1587848724152-CreateAppointments.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateAppointments1587848724152
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'timestamp',
+            isNullable: false,
+          },
+        ],
+      }),
+      true
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appointments');
+  }
+}
