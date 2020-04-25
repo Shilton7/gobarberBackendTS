0545373b4fee7355b211b36e2153612c922c654a
Shilton <shilton.sistemas@gmail.com>
2020-04-25 11:33:41 -0300

    Configuring Debug Node.js

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,4 +6,16 @@ routes.get('/', (request, response) =>
   response.json({ message: 'Hello Shilton' })
 );
 
+// test debug
+routes.post('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  };
+
+  return response.json(user);
+});
+
 export default routes;
