fa122582393e42147ece590c7f1905dd743c93ac
Shilton <shilton.sistemas@gmail.com>
2020-04-28 20:38:02 -0300

    Avatar upload

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
 import { getRepository } from 'typeorm';
-import UserModel from '../models/User';
 
+import UserModel from '../models/User';
+import uploadConfig from '../config/upload';
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.get('/', async (request, response) => {
   const usersRepository = getRepository(UserModel);
@@ -33,5 +38,27 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const uploadUserAvatar = new UpdateUserAvatarService();
+
+      const user = await uploadUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  }
+);
 
 export default usersRouter;
---
+++ src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,45 @@
+import fs from 'fs';
+import path from 'path';
+
+import { getRepository } from 'typeorm';
+import UserModel from '../models/User';
+import uploadConfig from '../config/upload';
+
+interface RequestDTO {
+  user_id: string;
+  avatarFilename: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({
+    user_id,
+    avatarFilename,
+  }: RequestDTO): Promise<UserModel> {
+    const usersRepository = getRepository(UserModel);
+
+    const user = await usersRepository.findOne(user_id);
+
+    if (!user) {
+      throw Error('Only authenticated users can change.');
+    }
+
+    // Deletar avatar atual
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    // Atualizando avatar
+    user.avatar = avatarFilename;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
